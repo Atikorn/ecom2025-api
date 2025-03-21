e0919d65efb5b76fbea1e17fd2396c894dbc2d8f
Atikorn <264318241004-st@rmutsb.ac.th>
2025-03-21 15:49:55 +0700

    change api from 5001 to vercel

--- routers/model.js
+++ routers/model.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { authCheck, adminCheck } = require('../middlewares/authCheck')
 const { createM, listM, removeM } = require('../controllers/model')
 
-//ENDPOINT model http://localhost:5001/api/category
+//ENDPOINT model https://ecom2025-api-ecru.vercel.app/api/category
 router.post('/createModel', authCheck, adminCheck, createM)
 router.get('/listModel', listM)
 router.delete('/model/:id', authCheck, adminCheck, removeM)
